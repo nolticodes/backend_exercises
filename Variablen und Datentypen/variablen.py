6de00c453af56c565b37d7581206c21dd0222fec
Denis Nolting <denis.nolting@gmx.de>
2026-09-01 13:17:19 +0200

    doc: basics variables and types

---
+++ Variablen und Datentypen/variablen.py
@@ -0,0 +1,27 @@
+# Code Ausführen in Terminal:
+# mit cmd + a ganzen Text markieren oder nur den gewünschten bereich markeiren
+# mit shift + enter code ausführen und in Terminal anzeigen
+
+
+# formatierter String
+# benötigen ein f im code
+name = "Denis"
+age = 29
+
+print(f"Ich bin {name} und {age} alt")
+
+
+# Variablen lassen sich in python in einer Zeile definieren
+name2, age2 = "Melodi", 26
+
+print(f"Ich bin {name2} und {age2} alt")
+
+
+# Variablen können den Datentypn wechseln 
+age3 = 18
+age3 = "Achtzehn"
+
+# Methode um sich alle keywords in python ausgeben zu lassen (dürfen icht als variablen namen verwendet werden)
+import keyword
+
+print(keyword)
