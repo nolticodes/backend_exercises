e1fb8efd2d334c080693db19ae773b35cae82dd2
Denis Nolting <denis.nolting@gmx.de>
2026-09-11 14:57:46 +0200

    add: http handing

--- Django/tech_gadgets/urls.py
+++ Django/tech_gadgets/urls.py
@@ -4,5 +4,5 @@ from .views import start_page_view, single_gadget_view, single_gadget_slug_view
 urlpatterns = [
     path('', start_page_view),
     path('gadget/<int:gadget_id>/', single_gadget_view),
-    path('gadget/<slug:gadget_slug>/', single_gadget_slug_view),
+    path('gadget/<slug:gadget_slug>/', single_gadget_slug_view, name="gadget_slug_url"),
 ]
--- Django/tech_gadgets/views.py
+++ Django/tech_gadgets/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse, HttpResponseNotFound, Http404
 
 from .dummy_data import gadgets
 
 from django.utils.text import slugify
+
+from django.urls import reverse
 
 # Create your views here.
 
@@ -11,12 +13,19 @@ def start_page_view(request):
     return HttpResponse("Hey das hat funktioniert!")
 
 def single_gadget_view(request, gadget_id):
-    return JsonResponse({"result": slugify(gadgets[1]['name'])})
+    if len(gadgets) > gadget_id:
+        new_slug = slugify(gadgets[gadget_id]["name"])
+        new_url = reverse("gadget_slug_url", args=[new_slug])
+        return redirect(new_url)
+    return HttpResponseNotFound("nicht gefunden du spacko")
 
 def single_gadget_slug_view(request, gadget_slug):
-    gadget_match = {"result": "nothing"}
+    gadget_match = None
 
     for gadget in gadgets: 
         if slugify(gadget["name"]) == gadget_slug:
             gadget_match = gadget
-    return JsonResponse(gadget_match)
+
+    if gadget_match:
+        return JsonResponse(gadget_match)
+    raise Http404("nichts da")
