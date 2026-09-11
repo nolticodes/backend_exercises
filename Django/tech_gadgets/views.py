0b072632e2f0f2d82970ab76a00d8dc85a3b3cde
Denis Nolting <denis.nolting@gmx.de>
2026-09-11 14:49:30 +0200

    feat: add slugs

--- Django/tech_gadgets/views.py
+++ Django/tech_gadgets/views.py
@@ -12,3 +12,11 @@ def start_page_view(request):
 
 def single_gadget_view(request, gadget_id):
     return JsonResponse({"result": slugify(gadgets[1]['name'])})
+
+def single_gadget_slug_view(request, gadget_slug):
+    gadget_match = {"result": "nothing"}
+
+    for gadget in gadgets: 
+        if slugify(gadget["name"]) == gadget_slug:
+            gadget_match = gadget
+    return JsonResponse(gadget_match)
