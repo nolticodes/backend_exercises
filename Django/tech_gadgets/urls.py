365351f059e61ccd0ecaaa0b06fadd0453f4eb0e
Denis Nolting <denis.nolting@gmx.de>
2026-09-11 14:38:46 +0200

    feat: add slugs

--- Django/tech_gadgets/urls.py
+++ Django/tech_gadgets/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import start_page_view
+from .views import start_page_view, single_gadget_view
 
 urlpatterns = [
     path('', start_page_view),
+    path('gadget/<int:gadget_id>/', single_gadget_view),
+    path('gadget/<str:gadget_id>/', single_gadget_view),
 ]
--- Django/tech_gadgets/views.py
+++ Django/tech_gadgets/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+from .dummy_data import gadgets
+
+from django.utils.text import slugify
 
 # Create your views here.
 
 def start_page_view(request):
     return HttpResponse("Hey das hat funktioniert!")
 
+def single_gadget_view(request, gadget_id):
+    return JsonResponse({"result": slugify(gadgets[1]['name'])})
