6a6bf3cce0f26d3e2b9f77e49d1ca45de33aaf30
Denis Nolting <denis.nolting@gmx.de>
2026-09-18 11:02:26 +0200

    feat: testing listview and detailview

--- Django Datenbank und Adminpanel/sales/urls.py
+++ Django Datenbank und Adminpanel/sales/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from .views import CustomerListView
+from .views import CustomerListSearchView, CustomerDetailView
 
-urlspattern = [
-
-    
+urlpatterns = [
+    path('', CustomerListView.as_view()),
+    path("<str:name>/", CustomerListSearchView.as_view(),),
+    path('customer/<int:pk>/', CustomerDetailView.as_view())
 ]
--- Django Datenbank und Adminpanel/sales/views.py
+++ Django Datenbank und Adminpanel/sales/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Customer
+from django.views.generic.detail import DetailView
 
 # Create your views here.
+
+class CustomerListView(ListView):
+    model = Customer
+    template_name = "sales/list.html"
+    paginate_by = 3
+
+class CustomerListSearchView(CustomerListView):
+    def get_queryset(self):
+        name = self.kwargs.get("name")
+        return Customer.objects.filter(first_name__icontains=name)
+
+class CustomerDetailView(DetailView):
+    model = Customer
+    template_name = "sales/detail.html"
