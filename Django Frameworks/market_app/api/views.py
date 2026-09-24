47252b15c2471981a86a5981296af9f4aafb3227
Denis Nolting <denis.nolting@gmx.de>
2026-09-24 17:10:58 +0200

    feat(market_app): add view

--- Django Frameworks/market_app/api/views.py
+++ Django Frameworks/market_app/api/views.py
@@ -1,6 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
-@api_view()
+
+@api_view(['GET', 'POST'])
 def first_view(request):
-    return Response({"message": "Moinsen Welt"})
+    if request.method == 'GET':
+        return Response({"message": "Moin world"})
+    if request.method == 'POST':
+        try:
+            msg = request.data['message']
+            return Response({"your_mesg": msg}, status=status.HTTP_201_CREATED)
+        except: 
+            return Response({"your_mesg": "error_grüße"}, status=status.HTTP_400_BAD_REQUEST)
+        
+    
