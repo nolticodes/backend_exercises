6a5340063ff605f94adadd976e057134fef5bc96
Denis Nolting <denis.nolting@gmx.de>
2026-09-25 12:55:32 +0200

    feat: swith to modelserializer

--- Django Frameworks/market_app/api/serializers.py
+++ Django Frameworks/market_app/api/serializers.py
@@ -15,24 +15,14 @@ def validate_noX(value):
 
         return value
 
-class MarketSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=255)
-    location = serializers.CharField(max_length=255, validators=[validate_noX])
-    description = serializers.CharField()
-    net_worth = serializers.DecimalField(max_digits=100, decimal_places=2)
+class MarketSerializer(serializers.ModelSerializer):
 
+    class Meta:
+         model = Market
+         fields = '__all__'
 
-    def create(self, validate_data):
-        return Market.objects.create(**validate_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.location = validated_data.get('location', instance.location)
-        instance.description = validated_data.get('description', instance.description)
-        instance.net_worth = validated_data.get('net_worth', instance.net_worth)
-        instance.save()
-        return instance
+    def validate_location(self, value):
+        return validate_noX(value)
 
 
 class SellerDetailsSerializer(serializers.Serializer):
