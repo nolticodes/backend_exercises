a233aafcf21848b9ecc9dd3b5975cecf58a3e7fb
Denis Nolting <denis.nolting@gmx.de>
2026-09-25 11:24:13 +0200

    feat(market_app): add seller model

--- Django Frameworks/market_app/api/urls.py
+++ Django Frameworks/market_app/api/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path, include
-from .views import market_view, markets_single_view
+from .views import market_view, markets_single_view, seller_view
 
 urlpatterns = [
-    path('', market_view),
-    path('<int:pk>/', markets_single_view)
+    path('market', market_view),
+    path('market/<int:pk>/', markets_single_view),
+    path('seller', seller_view)
 ]
